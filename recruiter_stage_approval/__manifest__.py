12afe0e9168572f1ff20550f78143fd525e5432b
jahidul17 <jahidulislam.khan1234@gmail.com>
2026-05-06 16:12:17 +0600

    new recruier app approval app custom user base

---
+++ recruiter_stage_approval/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Recruiter Approval',
+    'version': '19.0.1.0.0',
+    'summary': 'Multi-level approval process for Job Posts (1st stage -> 2nd stage -> Final Stage)',
+    'sequence': 10,
+    'description': """
+        This module adds a custom status flow for hr.job:
+        1. Draft
+        2. 1st stage 
+        3. 2nd stage
+        4. Final Stage
+    """,
+    'category': 'Education',
+    'author': 'Zahidul Islam', 
+    'license': 'LGPL-3',
+    'depends': ['base', 'hr_recruitment','website_hr_recruitment','mail'],
+    'data': [
+        'views/stage.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
