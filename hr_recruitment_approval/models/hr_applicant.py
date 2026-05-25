bc7f1ee8aa549414f9a8aaab18d31673c83d62e2
jahidul17 <jahidulislam.khan1234@gmail.com>
2026-05-25 14:31:04 +0600

    send email issue solved

--- hr_recruitment_approval/models/hr_applicant.py
+++ hr_recruitment_approval/models/hr_applicant.py
@@ -7,16 +7,16 @@ class HrApplicant(models.Model):
         return self.env.ref('hr_recruitment_approval.action_report_offer_letter').report_action(self)
     
     def action_send_offer_email(self):
-        template=self.env.ref('hr_recruitment_approval.email_template_offer_letter',False)
-        return{
-            'type':'ir.actions.act_window',
-            'view_mode':'form',
-            'res_model':'mail.compose.message',
-            'target':'new',
-            'context':{
-                'default_model':'hr.applicant',
-                'default_res_id':self.id,
-                'default_template_id':template.id,
-                'default_composition_mode':'comment',
+        template = self.env.ref('hr_recruitment_approval.email_template_offer_letter', False)
+        return {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'target': 'new',
+            'context': {
+                'default_model': 'hr.applicant',
+                'default_res_ids': [self.id], 
+                'default_template_id': template.id if template else False,
+                'default_composition_mode': 'comment',
             }
         }
